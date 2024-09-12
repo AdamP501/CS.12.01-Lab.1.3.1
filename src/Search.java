6fb09aca157fd608e3ebc93075dfa86ff2c53425
AdamP501 <axonp2026@student.cis.edu.hk>
2024-09-12 22:56:21 +0800

    finished

--- src/Search.java
+++ src/Search.java
@@ -4,25 +4,81 @@ public class Search {
 
     public static int linearSearch(int[] array, int value) {
 
-        return -1;
+        for (int i = 0; i < array.length; i++)
+        {
+            if (array[i] == value)
+            {
+                return i;
+            }
+        }
 
+        return -1;
     }
 
     public static int linearSearch(ArrayList<String> list, String string) {
 
-        return -1;
+        for (int i = 0; i < list.size(); i++){
+            if (list.get(i).equals(string))
+            {
+                return i;
+            }
+        }
 
+        return -1;
     }
 
     public static int binarySearch(int[] array, int value) {
+        int start = 0;
+        int end = array.length-1;
+        int mid = (start + end)/2;
+
+        while (start <= end)
+        {
+            mid = (start + end)/2;
+
+            if (array[mid] == value)
+            {
+                return mid;
+            }
+            else if (array[mid] < value)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
 
         return -1;
 
     }
 
     public static int binarySearch(String[] array, String string) {
+        int start = 0;
+        int end = array.length-1;
+        int mid = (start + end)/2;
+
+        while (start <= end)
+        {
+            mid = (start + end)/2;
+
+            if (array[mid].equals(string))
+            {
+                return mid;
+            }
+            else if (array[mid].compareTo(string) < 0)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
 
         return -1;
+
 
     }
 }
